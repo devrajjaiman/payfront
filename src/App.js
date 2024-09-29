6170652fe3a76e94840ddb1322c44338c22ec4d8
devrajjaiman <devraj.jaiman@gmail.com>
2024-09-29 09:29:46 +0530

    title and form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './paybump-banner.svg';
 import './App.css';
 
+import { FilloutStandardEmbed } from '@fillout/react';
+import '@fillout/react/style.css';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      {/* <header className="App-header"> */}
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p className='App-p'>
           Welcome to Paybump!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Submit your raise here!
-        </a>
-      </header>
+      <div
+        style={{
+          width: 400,
+          height: 700,
+        }}
+      >
+        <FilloutStandardEmbed filloutId="xmj8WHwcAvus" />
+      </div>
+    {/* </header> */}
     </div>
   );
 }
